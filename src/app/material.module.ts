bec2e74d4ad632358b9261bac36df0626d513366
Francisco <danielarcesoftware@gmail.com>
2021-06-16 23:47:25 -0700

    Navbar created with its functionality

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,10 +3,13 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
-  imports: [MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule],
-  exports: [MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule]
+  imports: [MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule,MatSidenavModule,MatToolbarModule,MatListModule],
+  exports: [MatButtonModule,MatIconModule,MatFormFieldModule,MatInputModule,MatSidenavModule,MatToolbarModule,MatListModule]
 })
 
 export class MaterialModule{}
