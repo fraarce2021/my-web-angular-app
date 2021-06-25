d140c53bd2c9ff3752b5db5ba2d2e3ad84a5bc3d
Francisco <danielarcesoftware@gmail.com>
2021-06-24 21:47:28 -0700

    Remove components books for hard reset

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
-import { BooksComponent } from './books/books.component';
 import { LoginComponent } from './security/login/login.component';
 import { RegisterComponent } from './security/register/register.component';
 import { SecurityRouter } from './security/security.router';
 
 const routes: Routes = [
   {path:'', component: HomeComponent, canActivate: [SecurityRouter]},
-  {path:'books', component: BooksComponent},
   {path:'login', component: LoginComponent},
   {path:'register', component: RegisterComponent},
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user.component';
 import { FormsModule } from '@angular/forms';
-import { BooksComponent } from './books/books.component';
-import { BookComponent } from './book/book.component';
-import { BooksService } from './services/books.service';
 import { HomeComponent } from './home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
@@ -22,8 +19,6 @@ import { SecurityService } from './security/security.service';
   declarations: [
     AppComponent,
     UserComponent,
-    BooksComponent,
-    BookComponent,
     HomeComponent,
     LoginComponent,
     RegisterComponent,
@@ -38,7 +33,7 @@ import { SecurityService } from './security/security.service';
     MaterialModule,
     FlexLayoutModule
   ],
-  providers: [BooksService, SecurityService ],
+  providers: [SecurityService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
